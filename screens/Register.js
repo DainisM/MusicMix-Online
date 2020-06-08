aee04826085a46426e30b827a9251d4b7919453b
Dainis Moisejenko <moisejenko11@gmail.com>
2020-06-08 09:54:31 +0200

    Done with Register screen (User can create new account)

--- screens/Register.js
+++ screens/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableWithoutFeedback, Picker, TouchableOpacity, Keyboard, Image, TextInput, ScrollView} from 'react-native';
+import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Keyboard, Image, TextInput, ScrollView} from 'react-native';
+import {Picker} from '@react-native-community/picker';
 import DatePicker from 'react-native-datepicker'
 
 export default class Register extends React.Component {
@@ -7,8 +8,108 @@ export default class Register extends React.Component {
         super(props);
 
         this.state = {
-            date: '',
-            gender: 'Male'
+            email: '',
+            username: '',
+            password: '',
+            confPassword: '', 
+            birthday: '',
+            gender: 'male',
+            emailError: "",
+            usernameError: "",
+            passwordError: "",
+            confPasswordError: "",
+            birthdayError: "",
+            registerMessage: ""
+        }
+    }
+
+    validate = () => {
+        let emailError = "";
+        let usernameError = "";
+        let passwordError = "";
+        let confPasswordError = "";
+        let birthdayError = "";
+
+        if (!this.state.email || !this.state.email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+            emailError = "Invalid email";
+        }
+
+        if (this.state.username.length > 10 || this.state.username.length < 3) {
+            usernameError = "Username must be between 3 and 10 characters long";
+        }
+
+        if (!this.state.password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
+            passwordError = "Password must contain at least 8 characters and contain at least 1 number, 1 uppercase and 1 lowercase character";
+        }
+
+        if (!this.state.password.match(this.state.confPassword)) {
+            confPasswordError = "Passwords does not match";
+        }
+
+        if (!this.state.birthday) {
+            birthdayError = "Please choose your birthday";
+        }
+
+        if (
+            emailError ||
+            usernameError ||
+            passwordError ||
+            confPasswordError ||
+            birthdayError
+            ) {
+            this.setState({
+                emailError,
+                usernameError,
+                passwordError,
+                confPasswordError,
+                birthdayError
+            });
+            return false;
+        }
+      
+          //Else return true
+          return true;
+    }
+
+    async signUp() {
+        const isValid = this.validate();
+
+        if (isValid) {
+            console.log('Email: '+this.state.email);
+            console.log('Username: '+this.state.username);
+            console.log('Password: '+this.state.password);
+            console.log('Confirm password: '+this.state.confPassword);
+            console.log('Gender: '+this.state.gender);
+            console.log('Birthday: '+this.state.birthday);
+
+            await fetch('http://api.music-mix.live/users/signup', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                username: this.state.username,
+                password: this.state.password,
+                gender: this.state.gender,
+                birthday: this.state.birthday
+            })
+            }).then((response) => {
+                    if (response.ok) {
+                        response.json().then(async (json) => {
+                            this.setState({registerMessage: 'User created successfully!'})
+
+                            setTimeout( () => {
+                                this.props.navigation.replace('Login');
+                            }, 2000) 
+                            
+                        })
+                    } else {
+                        this.setState({loginError: true})
+                    }
+                })
+                .catch((error) => console.log(error));
         }
     }
 
@@ -31,35 +132,56 @@ export default class Register extends React.Component {
                                 style={styles.regsterFormInput}
                                 autoCompleteType={'email'}
                                 autoCorrect={false} 
+                                onChangeText={(text) => this.setState({email: text, emailError: ''})}
+                                value={this.state.email}
                             />
                         </View>
+                        {!this.state.emailError == '' ? (
+                            <Text style={styles.ErrorText}>{this.state.emailError}</Text>
+                        ) : (null)}
+
 
                         <View style={styles.registerFormRow}>
                             <Text style={styles.registerFormLabel}>Username</Text>
                             <TextInput 
                                 style={styles.regsterFormInput}
                                 autoCompleteType={'username'}
-                                autoCorrect={false} 
+                                autoCorrect={false}
+                                onChangeText={(text) => this.setState({username: text, usernameError: ''})}
+                                value={this.state.username} 
                             />
                         </View>
+                        {!this.state.usernameError == '' ? (
+                            <Text style={styles.ErrorText}>{this.state.usernameError}</Text>
+                        ) : (null)}
 
                         <View style={styles.registerFormRow}>
                             <Text style={styles.registerFormLabel}>Password</Text>
                             <TextInput 
                                 style={styles.regsterFormInput}
                                 autoCompleteType={'password'} 
-                                secureTextEntry={true} 
+                                secureTextEntry={true}
+                                onChangeText={(text) => this.setState({password: text, passwordError: ''})}
+                                value={this.state.password} 
                             />
                         </View>
+                        {!this.state.passwordError == '' ? (
+                            <Text style={styles.ErrorText}>{this.state.passwordError}</Text>
+                        ) : (null)}
 
                         <View style={styles.registerFormRow}>
                             <Text style={styles.registerFormLabel}>Confirm Password</Text>
                             <TextInput 
                                 style={styles.regsterFormInput}
                                 autoCompleteType={'password'} 
-                                secureTextEntry={true} 
+                                secureTextEntry={true}
+                                onChangeText={(text) => this.setState({confPassword: text, confPasswordError: ''})}
+                                value={this.state.confPassword} 
                             />
                         </View>
+                        {!this.state.confPasswordError == '' ? (
+                            <Text style={styles.ErrorText}>{this.state.confPasswordError}</Text>
+                        ) : (null)}
 
                         <View style={styles.registerFormRow}>
                             <Text style={styles.registerFormLabel}>Gender</Text>
@@ -69,7 +191,7 @@ export default class Register extends React.Component {
                                 selectedValue={this.state.gender}
                                 onValueChange={(itemValue, itemIndex) => this.setState({gender: itemValue})}
                             >
-                                <Picker.Item label='Male' vaue='male'/>
+                                <Picker.Item label='Male' value='male'/>
                                 <Picker.Item label='Female' value='female'/>
                             </Picker>
                         </View>
@@ -78,10 +200,11 @@ export default class Register extends React.Component {
                             <Text style={styles.registerFormLabel}>Birthday</Text>
                             <DatePicker
                                 style={{width: '40%'}}
-                                date={this.state.date}
+                                date={this.state.birthday}
                                 mode="date"
                                 placeholder="select date"
                                 format="YYYY-MM-DD"
+                                maxDate="2020-01-01"
                                 confirmBtnText="Confirm"
                                 cancelBtnText="Cancel"
                                 customStyles={{
@@ -95,21 +218,27 @@ export default class Register extends React.Component {
                                     marginLeft: 36
                                 }
                                 }}
-                                onDateChange={(date) => {this.setState({date: date})}}
+                                onDateChange={(date) => {this.setState({birthday: date, birthdayError: ''})}}
                             />
                         </View>
+                        {!this.state.birthdayError == '' ? (
+                            <Text style={styles.ErrorText}>{this.state.birthdayError}</Text>
+                        ) : (null)}
                         
 
                     </View>
 
 
                     <View style={styles.registerNavigationContainer}>
-                        <TouchableOpacity style={styles.registerButton} >
+                        <TouchableOpacity style={styles.registerButton} onPress={() => this.signUp()}>
                             <Text style={styles.reigsterButtonText}>Sign up</Text>
                         </TouchableOpacity>
+                        {!this.state.registerMessage == '' ? (
+                            <Text style={styles.signupSuccess}>{this.state.registerMessage}</Text>
+                        ) :(null)}
         
                         <TouchableOpacity
-                            style={{marginTop: '5%'}} 
+                            style={{marginTop: '3%'}} 
                             onPress={() => this.props.navigation.navigate('Login')}
                         >
                             <Text style={styles.navigatoBackToLogin}>Already have an account? Log in now!</Text>
@@ -134,7 +263,7 @@ const styles = StyleSheet.create({
         width: '100%',
         alignItems: 'center',
         elevation: 3,
-        marginBottom: '5%',
+        marginBottom: '3%',
     },
     registerForm: {
         flexDirection: 'column',
@@ -146,7 +275,11 @@ const styles = StyleSheet.create({
     registerFormRow: {
         flexDirection: 'row',
         width: '100%',
-        marginVertical: '2%',
+        marginVertical: '1%',
+    },
+    ErrorText: {
+        alignSelf: 'center',
+        color: 'darkred',
     },
     registerFormLabel: {
         width: '40%',
@@ -166,7 +299,7 @@ const styles = StyleSheet.create({
     registerNavigationContainer: {
         alignItems: 'center',
         height: '10%',
-        marginVertical: '5%',
+        marginVertical: '3%',
     },
     navigatoBackToLogin: {
         fontSize: 18,
@@ -186,4 +319,11 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 20,
     },
+    signupSuccess: {
+        marginTop: '2%',
+        color: 'darkgreen',
+        fontWeight: 'bold',
+        fontSize: 16,
+
+    }
 });
