14d482acc3d811a327e3d65237911a0ee56651d5
Dainis Moisejenko <moisejenko11@gmail.com>
2020-06-09 14:32:40 +0200

    Updated Home screen navigation with bottomTabNavigator

--- components/routes/homeStack.js
+++ components/routes/homeStack.js
@@ -1,17 +1,57 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text } from 'react-native';
+import { createBottomTabNavigator  } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Browse from '../../screens/Browse';
 import Search from '../../screens/Search';
 import Library from '../../screens/Library';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator (); 
 
 export default function HomeStack() {
     return (
-            <Stack.Navigator initialRouteName='Browse' screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Browse" component={Browse}/>
-                <Stack.Screen name="Search" component={Search}/>
-                <Stack.Screen name="Library" component={Library}/>
-            </Stack.Navigator>
+            <Tab.Navigator initialRouteName='Browse'  
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                    if (route.name === 'Browse') {
+                        return (
+                        <Icon
+                            name='home'
+                            size={40}
+                            color={focused ? 'cyan': 'darkcyan'}
+                        />
+                        );
+                    } else if (route.name === 'Search') {
+                        return (
+                        <Icon
+                            name='search'
+                            size={30}
+                            color={focused ? 'cyan': 'darkcyan'}
+                        />
+                        );
+                    }
+                    else if (route.name === 'Library') {
+                        return (
+                        <Icon
+                            name='list'
+                            size={30}
+                            color={focused ? 'cyan': 'darkcyan'}
+                        />
+                        );
+                    }
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'cyan',
+                    inactiveTintColor: 'darkcyan',
+                    style: {backgroundColor: '#002930'}
+                }}
+                >
+                <Tab.Screen name="Browse" component={Browse}/>
+                <Tab.Screen name="Search" component={Search}/>
+                <Tab.Screen name="Library" component={Library}/>
+            </Tab.Navigator>
     )
 }
+
+// screenOptions={{headerShown: false}}
--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import HomeStack from '../components/routes/homeStack';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -18,38 +17,6 @@ export default class Home extends React.Component {
                 
                 <HomeStack />
 
-                <View style={styles.homeNavigationContainer}>
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Browse')}  style={[styles.homeNavigationIcons]}>
-                        <Icon
-                            name='home' 
-                            backgroundColor={'inherit'} 
-                            color={'darkcyan'} 
-                            size={40}
-                            style={{padding: 5}}
-                        />
-                    </TouchableOpacity>
-                    
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Search')}  style={styles.homeNavigationIcons}>
-                        <Icon
-                            name='search' 
-                            backgroundColor={'inherit'} 
-                            color={'darkcyan'} 
-                            size={40}
-                            style={{padding: 5}}
-                        />
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Library')} style={styles.homeNavigationIcons}>
-                        <Icon                            
-                            name='list' 
-                            backgroundColor={'inherit'} 
-                            color={'darkcyan'} 
-                            size={40}
-                            style={{padding: 5}}
-                        />
-                    </TouchableOpacity>
-                </View>
-
             </View>
         );
     }
@@ -60,19 +27,4 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#002930',
     },
-    homeNavigationContainer: {
-        height: '10%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: '10%',
-        paddingVertical: '3%',
-        borderTopWidth: 3,
-        borderTopColor: 'darkcyan',
-    },
-    homeNavigationIcons: {
-        width: 60, 
-        borderBottomColor: 'cyan', 
-        alignSelf: 'center',
-        alignItems: 'center',
-    }
 });
